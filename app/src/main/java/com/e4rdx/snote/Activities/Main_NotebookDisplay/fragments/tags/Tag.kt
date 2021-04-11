d20d019a41283f4fa9fde1df4778dcd5d5665a30
e4rdx <nomail@noprovider.com>
2021-04-11 10:23:27 +0200

    Popup to create Tags, display all tags in flowlayout

--- app/src/main/java/com/e4rdx/snote/Activities/Main_NotebookDisplay/fragments/tags/Tag.kt
+++ app/src/main/java/com/e4rdx/snote/Activities/Main_NotebookDisplay/fragments/tags/Tag.kt
@@ -3,10 +3,49 @@
 package com.e4rdx.snote.Activities.Main_NotebookDisplay.fragments.tags
 
 import android.content.Context
+import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.e4rdx.snote.FlowLayout
+import com.e4rdx.snote.R
 
-class Tag(context: Context, name: String): LinearLayout(context) {
+class Tag(context: Context, name: String, ref: TagFragment): LinearLayout(context) {
+    var name: String? = null
+    var fragmentRef: TagFragment? = null
+
     init {
+        this.orientation = HORIZONTAL
+        this.setBackgroundResource(R.drawable.background_tag)
+        //this.setBackgroundColor(Color.parseColor("#FF0000"))
 
+        fragmentRef = ref
+
+        var params = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        this.layoutParams = params
+
+        this.name = name
+
+        val txt = TextView(context)
+        txt.text = name
+        txt.textSize = 20F
+        txt.setTextColor(ContextCompat.getColor(context, R.color.black))
+        params = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        params.leftMargin = 20
+        txt.layoutParams = params
+
+
+        val img_btn = ImageButton(context)
+        img_btn.setBackgroundResource(R.drawable.ic_close)
+        params = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+        img_btn.layoutParams = params
+        img_btn.setOnClickListener{
+            val parentLinearLayout = this.parent as FlowLayout
+            parentLinearLayout.removeView(this)
+            fragmentRef!!.saveTags()
+        }
+
+        this.addView(txt)
+        this.addView(img_btn)
     }
 }
--- app/src/main/java/com/e4rdx/snote/Activities/Main_NotebookDisplay/fragments/tags/TagFragment.kt
+++ app/src/main/java/com/e4rdx/snote/Activities/Main_NotebookDisplay/fragments/tags/TagFragment.kt
@@ -2,11 +2,18 @@
 
 package com.e4rdx.snote.Activities.Main_NotebookDisplay.fragments.tags
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.text.InputType
 import android.view.*
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.e4rdx.snote.Activities.Main_NotebookDisplay.NotebookDisplayer
+import com.e4rdx.snote.FlowLayout
 import com.e4rdx.snote.R
+import com.e4rdx.snote.utils.SNoteManager
+import org.json.JSONArray
+import org.json.JSONException
 
 class TagFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -18,14 +25,82 @@ class TagFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        loadTags()
+    }
+
+    override fun onDestroy() {
+        //saveTags()
+        super.onDestroy()
+    }
+
+    override fun onStop() {
+        //saveTags()
+        super.onStop()
+    }
+
+    fun saveTags(){
+        val jsonObj = SNoteManager.readNoteFile(requireContext())
+        val linearLayoutTags = requireActivity().findViewById<FlowLayout>(R.id.tags_flowlayout)
+        val tags = JSONArray()
+        if(linearLayoutTags.childCount > 0) {
+            for (i in 0..linearLayoutTags.childCount) {
+                if( linearLayoutTags.getChildAt(i) != null) {
+                    val currentTag = linearLayoutTags.getChildAt(i) as Tag
+                    tags.put(currentTag.name)
+                }
+            }
+        }
+        jsonObj.put("tags", tags)
+        SNoteManager().saveCurrent(jsonObj.toString(), requireContext())
+    }
+
+    private fun loadTags(){
+        val linearLayoutTags = requireActivity().findViewById<FlowLayout>(R.id.tags_flowlayout)
+
+        val jsonObj = SNoteManager.readNoteFile(requireContext())
+        try {
+            val jsonTags: JSONArray? = jsonObj.getJSONArray("tags")
+            for (i in 0..jsonTags!!.length()) {
+                linearLayoutTags.addView(Tag(requireContext(), jsonTags.getString(i), this))
+            }
+        } catch (e: JSONException){
+
+        }
+    }
+
+    private fun newTagDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("New Tag")
+        builder.setMessage("Enter the name for the new tag")
+
+        // Set up the input
+        val input = EditText(requireContext())
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        builder.setView(input)
+
+        // Set up the buttons
+        builder.setPositiveButton("Create") { dialog, which ->
+                val flowlayoutTags = requireActivity().findViewById<FlowLayout>(R.id.tags_flowlayout)
+                flowlayoutTags.addView(Tag(requireContext(), input.text.toString(), this))
+                saveTags()
+        }
+        builder.setNegativeButton("Cancel") { dialog, which -> dialog.cancel() }
+        builder.show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_tag, menu);
-        super.onCreateOptionsMenu(menu,inflater);
+        inflater.inflate(R.menu.menu_tag, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_tag_addTag -> {
+                newTagDialog()
+            }
+        }
         return super.onOptionsItemSelected(item)
     }
 }
